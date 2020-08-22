1c730c7b25d0e0f65829a9d754bf597251e7b9db
ZacharyHinds <67064643+ZacharyHinds@users.noreply.github.com>
2020-08-22 11:48:16 -0400

    Made color more green

--- lua/terrortown/entities/roles/spectre/shared.lua
+++ lua/terrortown/entities/roles/spectre/shared.lua
@@ -5,7 +5,7 @@ if SERVER then
 end
 
 function ROLE:PreInitialize()
-	self.color = Color(0, 242, 255, 255)
+	self.color = Color(0, 26, 188, 156)
 
 	self.abbr = "spr" -- abbreviation
 	self.surviveBonus = 0 -- bonus multiplier for every survive while another player was killed
